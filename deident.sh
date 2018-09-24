207d36edcd4deef3d329254bf7f3cd21aadf69b5
elizzak <elizzak@gmail.com>
2018-09-24 10:58:55 -0500

    Add files via upload

--- deident.sh
+++ deident.sh
@@ -13,6 +13,14 @@ if [ ! -e $project_name.txt ]; then
 echo -n $project_name"001" >> $project_name.txt
 fi
 
+#ssh leela > rm -rf myname.cfg
+# make a list of patients on command line to do them all in a row
+# keep project name folder and keyflies in EXPORT folder, not anonymization
+
+# Keep a record of all the activity done by users on this server?
+
+
+
 
 # # create a key to initiate anonymization
 python ../key_create.py -N $orig_folder -P $project_name
@@ -20,15 +28,8 @@ cd ../
 
 # move data to leela server for anonymization
 # usage: storescu [options] peer port dcmfile-in...
-storescu -aec ${project_name}/${orig_folder} --propose-lossless +sd +r 141.106.208.32 8105 $orig_folder
+storescu -aec ${project_name} --propose-lossless +sd +r 141.106.208.32 8105 $orig_folder
 
 # # place copy of config file and key file in project_name folder on leela
 scp ${project_name}/patients.map ezak@141.106.208.32:/data/scpForward/Output/${project_name} ## this line must be first
 scp myname.cfg ezak@141.106.208.32:/data/scpForward/Output/${project_name} ## this line must be last
-
-# # create new folder and store anonymized data
-# #read file newname.txt and get new name
-# cd $project_name
-# newname=`cat newname.txt`
-# #mkdir newname
-# #scp back from leela into newname
